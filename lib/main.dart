f8e0fbf0b353c38876cfee83ae89ea660799185f
nekomangini <encar.luis.salazar@gmail.com>
2024-09-05 08:55:00 +0800

    UI: removed bottomNavigationBar widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
-import 'package:blender_keys/screens/about.dart';
-import 'package:blender_keys/screens/home_screen.dart';
-import 'package:blender_keys/strings/content_strings.dart';
-import 'package:blender_keys/themes/color.dart';
+import 'package:blenderkeys_v2/screens/home_screen.dart';
+import 'package:blenderkeys_v2/strings/content_strings.dart';
+import 'package:blenderkeys_v2/themes/color.dart';
 import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(const SplashPage());
 
@@ -55,39 +51,44 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   // int pageIndex = 0;
 
   // ################################################################ //
   // pages routes //
-  final pages = [
-    const HomeScreen(),
-    const Donate(),
-  ];
+  // final pages = [
+  //   const HomeScreen(),
+  //   const Donate(),
+  // ];
 
   // ################################################################ //
   // icons of pages //
-  List<IconData> iconList = [
-    // list of icons that required by animated navigation bar.
-    Icons.home,
-    FontAwesomeIcons.addressBook
-  ];
+  //  List<IconData> iconList = [
+  //    // list of icons that required by animated navigation bar.
+  //    Icons.home,
+  //    FontAwesomeIcons.addressBook
+  //  ];
 
-  int _bottomNavIndex = 0;
+  //  int _bottomNavIndex = 0;
 
   // ################################################################ //
   // open url in browser //
-  Future<void> _launchUrl(String url) async {
-    // the host parameter is used to specify the hostname of the URL (e.g. "www.youtube.com"), not the entire URL.
-    // You should use the path parameter instead to specify the full URL.
-    final Uri uri = Uri(scheme: "https", host: url);
+  // Future<void> _launchUrl(String url) async {
+  //   // the host parameter is used to specify the hostname of the URL (e.g. "www.youtube.com"), not the entire URL.
+  //   // You should use the path parameter instead to specify the full URL.
+  //   final Uri uri = Uri(scheme: "https", host: url);
 
-    if (!await launchUrl(
-      uri,
-      mode: LaunchMode.externalApplication,
-    )) {
-      throw "Cannot launch url";
-    }
-  }
+  //   if (!await launchUrl(
+  //     uri,
+  //     mode: LaunchMode.externalApplication,
+  //   )) {
+  //     throw "Cannot launch url";
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -105,36 +106,39 @@ class _MainPageState extends State<MainPage> {
         centerTitle: true,
         backgroundColor: kAppbarBackgroundColor, // Color(0xFFff6900)
       ),
-      body: pages[_bottomNavIndex], //destination screen
+      // body: pages[_bottomNavIndex],
+      body: const HomeScreen(),
+      //destination screen
       extendBody: true,
 
       // ################################################################ //
       // icon in the middle //
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: kBodyBackgroundColor,
-        onPressed: () {
-          _launchUrl('www.blender.org');
-        },
-        child: Image.asset('images/blender_icon.png'),
-        //params
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      //  floatingActionButton: FloatingActionButton(
+      //    backgroundColor: kBodyBackgroundColor,
+      //    elevation: 0,
+      //    onPressed: () {
+      //      _launchUrl('www.blender.org');
+      //    },
+      //    child: Image.asset('images/blender_icon.png'),
+      //    //params
+      //  ),
+      //  floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 
       // ################################################################ //
       // bottom navigation bar properties //
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        elevation: 0,
-        inactiveColor: kInactiveIconColor,
-        backgroundColor: kBodyBackgroundColor,
-        icons: iconList,
-        activeIndex: _bottomNavIndex,
-        gapLocation: GapLocation.center,
-        notchSmoothness: NotchSmoothness.verySmoothEdge,
-        leftCornerRadius: 21,
-        rightCornerRadius: 21,
-        onTap: (index) => setState(() => _bottomNavIndex = index),
-        //other params
-      ),
+      // bottomNavigationBar: AnimatedBottomNavigationBar(
+      //   elevation: 0,
+      //   inactiveColor: kInactiveIconColor,
+      //   backgroundColor: kBodyBackgroundColor,
+      //   icons: iconList,
+      //   activeIndex: _bottomNavIndex,
+      //   gapLocation: GapLocation.center,
+      //   notchSmoothness: NotchSmoothness.verySmoothEdge,
+      //   leftCornerRadius: 21,
+      //   rightCornerRadius: 21,
+      //   onTap: (index) => setState(() => _bottomNavIndex = index),
+      //   //other params
+      // ),
     );
   }
 }
