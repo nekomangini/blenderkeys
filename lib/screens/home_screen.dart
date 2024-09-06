a42d78e731f68fffaa09a7e1b77a79463980c3be
nekomangini <encar.luis.salazar@gmail.com>
2024-09-06 10:25:03 +0800

    Updated the home screen and added padding to prevent widgets from touching the edge of the phone

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,78 +21,82 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBodyBackgroundColor,
-      body: ListView(
-        children: const [
-          // ################################################################ //
-          // HomeScreenContents is a user created widgets //
-          // that has page route, title text, icon and subtitle text property //
-          HomeScreenContents(
-            pageRoute: AnimationBlender(),
-            subScreenTitle: animationStringTitle,
-            customIcon: Icons.animation,
-            subText: animationSub,
-          ),
-          HomeScreenContents(
-            pageRoute: GeneralBlender(),
-            subScreenTitle: generalStringTitle,
-            customIcon: Icons.view_agenda,
-            subText: generalSub,
-          ),
-          HomeScreenContents(
-            pageRoute: GeoNodesBlender(),
-            subScreenTitle: geoNodesStringTitle,
-            customIcon: Icons.stadium,
-            subText: geometrySub,
-          ),
-          HomeScreenContents(
-            pageRoute: GreasePencilBlender(),
-            subScreenTitle: greasePencilStringTitle,
-            customIcon: Icons.draw,
-            subText: greaseSub,
-          ),
-          HomeScreenContents(
-            pageRoute: ModelingBlender(),
-            subScreenTitle: modelingStringTitle,
-            customIcon: Icons.monitor,
-            subText: modelingSub,
-          ),
-          HomeScreenContents(
-            pageRoute: RenderingBlender(),
-            subScreenTitle: renderingStringTitle,
-            customIcon: Icons.smart_screen,
-            subText: renderingSub,
-          ),
-          HomeScreenContents(
-            pageRoute: RiggingBlender(),
-            subScreenTitle: riggingStringTitle,
-            customIcon: Icons.skateboarding,
-            subText: riggingSub,
-          ),
-          HomeScreenContents(
-            pageRoute: SculptingBlender(),
-            subScreenTitle: sculptingStringTitle,
-            customIcon: Icons.satellite,
-            subText: sculptingSub,
-          ),
-          HomeScreenContents(
-            pageRoute: ShadingBlender(),
-            subScreenTitle: shadingStringTitle,
-            customIcon: Icons.roller_shades,
-            subText: shadingSub,
-          ),
-          HomeScreenContents(
-            pageRoute: TexturePaintBlender(),
-            subScreenTitle: texturePainStringTitle,
-            customIcon: Icons.format_paint,
-            subText: texturepaintSub,
-          ),
-          HomeScreenContents(
-            pageRoute: UvEditBlender(),
-            subScreenTitle: uvEditStringTitle,
-            customIcon: Icons.image,
-            subText: uvSub,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: const [
+            // ################################################################ //
+            // HomeScreenContents is a user created widgets //
+            // that has page route, title text, icon and subtitle text property //
+            HomeScreenContents(
+              pageRoute: AnimationBlender(),
+              subScreenTitle: animationStringTitle,
+              customIcon: Icons.animation,
+              subText: animationSub,
+            ),
+            HomeScreenContents(
+              pageRoute: GeneralBlender(),
+              subScreenTitle: generalStringTitle,
+              customIcon: Icons.view_agenda,
+              subText: generalSub,
+            ),
+            HomeScreenContents(
+              pageRoute: GeoNodesBlender(),
+              subScreenTitle: geoNodesStringTitle,
+              customIcon: Icons.stadium,
+              subText: geometrySub,
+            ),
+            HomeScreenContents(
+              pageRoute: GreasePencilBlender(),
+              subScreenTitle: greasePencilStringTitle,
+              customIcon: Icons.draw,
+              subText: greaseSub,
+            ),
+            HomeScreenContents(
+              pageRoute: ModelingBlender(),
+              subScreenTitle: modelingStringTitle,
+              customIcon: Icons.monitor,
+              subText: modelingSub,
+            ),
+            HomeScreenContents(
+              pageRoute: RenderingBlender(),
+              subScreenTitle: renderingStringTitle,
+              customIcon: Icons.smart_screen,
+              subText: renderingSub,
+            ),
+            HomeScreenContents(
+              pageRoute: RiggingBlender(),
+              subScreenTitle: riggingStringTitle,
+              customIcon: Icons.skateboarding,
+              subText: riggingSub,
+            ),
+            HomeScreenContents(
+              pageRoute: SculptingBlender(),
+              subScreenTitle: sculptingStringTitle,
+              customIcon: Icons.satellite,
+              subText: sculptingSub,
+            ),
+            HomeScreenContents(
+              pageRoute: ShadingBlender(),
+              subScreenTitle: shadingStringTitle,
+              customIcon: Icons.roller_shades,
+              subText: shadingSub,
+            ),
+            HomeScreenContents(
+              pageRoute: TexturePaintBlender(),
+              subScreenTitle: texturePainStringTitle,
+              customIcon: Icons.format_paint,
+              subText: texturepaintSub,
+            ),
+            HomeScreenContents(
+              pageRoute: UvEditBlender(),
+              subScreenTitle: uvEditStringTitle,
+              customIcon: Icons.image,
+              subText: uvSub,
+            ),
+            SizedBox(height: 10),
+          ],
+        ),
       ),
     );
   }
